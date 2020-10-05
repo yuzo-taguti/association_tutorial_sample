f54adfac80d972287cb3fccef2d25d29993933c4
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-10-05 22:06:48 +0900

    favoriteモデル・コントローラー作成とマイグレート

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
   root "tweets#index"
 
   devise_for :users
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_05_123629) do
+ActiveRecord::Schema.define(version: 2020_10_05_130431) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "tweet_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.text "body"
