807b55677bcdfa771eba08f4d31d332c323e0cac
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-10-05 21:56:22 +0900

    動作確認１回目OK

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,15 @@
 class TweetsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+
   def new
     @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user_id = current_user.id
+    @tweet.save
+    redirect_to tweets_path
   end
 
   def index
@@ -12,6 +20,9 @@ class TweetsController < ApplicationController
     @tweet = Tweet.find(params[:id])
   end
 
-  def create
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "tweews#index"
+  root "tweets#index"
 
   devise_for :users
   resources :tweets
