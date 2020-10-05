bd8013208703e7d27c41f9454bd936ca7e2fd0a7
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-10-05 21:44:02 +0900

    tweet/user　コントローラー記述

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,15 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
   def new
+    @tweet = Tweet.new
   end
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
+    @tweet = Tweet.find(params[:id])
   end
 
   def create
