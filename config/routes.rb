32736c64d48823fe11798a5a0e08d34e2d963222
t-yuzo <taguchiyuuzou@My-MBP.local>
2020-10-05 21:39:59 +0900

    ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
-  get 'tweets/new'
-  get 'tweets/index'
-  get 'tweets/show'
-  get 'tweets/create'
-  get 'users/index'
-  get 'users/show'
+  root "tweews#index"
+
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :tweets
+  resources :users
 end
